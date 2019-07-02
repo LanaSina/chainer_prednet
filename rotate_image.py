7504a510dbdafd1722816383dcd3467f531b4809
DESKTOP-7L9MC4G\lana <lana.sinapayen@gmail.com>
2019-07-02 09:52:07 +0900

    debug rotation

--- rotate_image.py
+++ rotate_image.py
@@ -2,7 +2,7 @@ import os
 from PIL import Image
 
 # Create an Image object from an Image
-input_image  = Image.open("./benham.jpg")
+input_image  = Image.open("./clean_disk.png")
 output_directory = "./benham_frames/"
 
 if not os.path.exists(output_directory):
@@ -14,6 +14,13 @@ rotation = 20
 rotation_direction = 1
 
 n = 360//rotation
+#rotated = input_image #.convert('RGB')
+non_transparent = Image.new('RGB',input_image.size,(255,255,255))
+non_transparent.paste(input_image,(0,0), mask = input_image)
+non_transparent.save("./benham_frames/0.jpg")
+
 for i in range(1,n):
-	rotated = input_image.rotate(rotation_direction*rotation)
-	rotated.save("./benham_frames/"+  str(i) + ".jpg")
+	rotated = input_image.rotate(rotation_direction*i*rotation)
+	non_transparent = Image.new('RGB',input_image.size,(255,255,255))
+	non_transparent.paste(rotated,(0,0), mask = rotated)
+	non_transparent.save("./benham_frames/"+  str(i) + ".jpg")
