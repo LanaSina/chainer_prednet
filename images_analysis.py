c575451fc9063cae2f6f5f87eaaccb22e5b4ca61
Lana <lana.sinapayen@gmail.com>
2019-07-05 15:53:59 +0900

    debugged

--- images_analysis.py
+++ images_analysis.py
@@ -10,11 +10,12 @@ if not os.path.exists(output_dir):
 
 image_list = sorted(os.listdir(image_dir))
 image_count = 20 # or image_list.length
+n = 0
 
 # read all images but last one
 for image_file in image_list[:image_count]:
-	n = 0
 	image_path = os.path.join(image_dir, image_file)
+	print("read ", image_path)
 
 	current_image = numpy.array(Image.open(image_path))
 	next_image = numpy.array(Image.open(image_path))
@@ -28,7 +29,9 @@ for image_file in image_list[:image_count]:
 			new_image[i, j, 0] = rdiff
 			bdiff = next_image[i, j, 2] - current_image[i, j, 2]
 			new_image[i, j, 2] = bdiff
-			image_array = Image.fromarray(new_image.astype('uint8'), 'RGB')
-			image_array.save(output_dir + str(n).zfill(3) + ".png")
+
+	image_array = Image.fromarray(new_image.astype('uint8'), 'RGB')
+	image_array.save(output_dir + str(n).zfill(3) + ".png")
+	print("saved image ", n)
 
 	n = n + 1
