5c9f3016487a59caf8fa6f700f855d12dbeaf841
Lana <lana.sinapayen@gmail.com>
2019-08-04 23:23:53 +0900

    debug typing and row/col order

--- images_analysis.py
+++ images_analysis.py
@@ -1,5 +1,6 @@
 import argparse
 import csv
+import cv2
 import numpy
 import os
 from PIL import Image
@@ -128,11 +129,11 @@ def red_blue_diff(current_image, next_image):
 # count the ratio of blue and red adter bblack-white alternation in a pixel
 def black_white_next(image_dir, output_dir):
 	image_list = sorted(os.listdir(image_dir))
-	image_count = 50
+	image_count = len(image_list)
 
 	# choose some random pixels
 	w = 160
-	h = 120
+	h = 90
 	pixels_count = w*h
 	coordinates = numpy.zeros((pixels_count,2))
 
@@ -142,9 +143,9 @@ def black_white_next(image_dir, output_dir):
 	# 	# col
 	# 	coordinates[i][1] = random.randrange(0,w)
 
-	for i in xrange(0,h):
-		for j in xrange(0,w):
-			n = i*h + j
+	for i in range(0,h):
+		for j in range(0,w):
+			n = i*w + j
 			# row
 			coordinates[n][0] = i
 			# col
@@ -156,26 +157,37 @@ def black_white_next(image_dir, output_dir):
 
 	b_t = 200
 	w_t = 55
-	blue_t = 5
-	red_t = 5
-
+	blue_t = 0
+	red_t = 0
+	variation = 0.0
+	col_mean = 0.0
 
 	#better to open several files... but how
-	output_file = "fpsi.csv"
+	output_file = "bike_15m.csv"
 	fieldnames = ['bw_r','bw_b','wb_r','wb_b']
+
+	# output_file = "fpsi_transitions.csv"
+	# fieldnames = ['black_tr', 'white_tr']
 	with open(output_dir+output_file, mode='w') as csv_file:
 		writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 		writer.writerow(fieldnames)
 
-		for image_file in image_list: #[:image_count]:
+		for image_file in image_list[:image_count]:
 			image_path = os.path.join(image_dir, image_file)
 			#print("read ", image_path)
 
 			# Beware: numpy ordering is b, g, r
 			# or is it
-			current_image = numpy.array(Image.open(image_path).convert('RGB'))
-			# new_image = numpy.ones(current_image.shape)*255
+			# h, w, color
+			# current_image = numpy.array(Image.open(image_path).convert('RGB'))
+			# print(current_image.shape)
 
+			current_image = cv2.imread(image_path)
+			current_image = cv2.cvtColor(current_image, cv2.COLOR_BGR2RGB) 
+			# print(current_image.shape)
+
+			# w, h ...
+			new_image = numpy.ones(current_image.shape)*255
 			pixel_index = 0
 			# (black->white , white->black)
 			blue_sum = [0,0]
@@ -191,7 +203,7 @@ def black_white_next(image_dir, output_dir):
 				r = 1.0*current_image[i, j, 0]
 				g = 1.0*current_image[i, j, 1]
 				b = 1.0*current_image[i, j, 2]
-
+			
 				# shift color measures
 				for time_index in range(0,t-1):
 					black[pixel_index][time_index] = black[pixel_index][time_index+1]
@@ -201,26 +213,34 @@ def black_white_next(image_dir, output_dir):
 				white[pixel_index][t-1] = 0
 				black[pixel_index][t-1] = 0
 
-				variation = (abs(r-g) + abs(g-b))/2.0
+				variation = (abs(r-g) + abs(g-b) + abs(b-r))/3.0
 				col_mean = (r + g + b)/3.0
-				#print(variation, col_mean)
 				if variation < 15:
-					#new_image[i,j] = current_image[i, j]
+					# if j > 90:
+					# 	print(current_image[i, j])
+					# 	print(col_mean)
 					# is it dark or pale?
-					if (col_mean < (255/2.0)):
+					if (col_mean > (255.0/2) ):
 						#pale
-						white[pixel_index][2] = 1
+						white[pixel_index][t-1] = 1
 					else:
-						if (col_mean > (255.0/2)):
-							black[pixel_index][2] = 1
+						if (col_mean < (255.0/2)):
+							black[pixel_index][t-1] = 1
 
 				# blue
-				if ( (b > r) and (b > g) ):
-					blue_plus = b - (r+g)/2.0
-				# red
-				else:
-					if ( (r > b) and (r > g ) ):
-						red_plus = r - (b+g)/2.0	
+				blue_plus = 0.0
+				red_plus = 0.0
+				# if ( (b > r) and (b > g) ):
+				# 	blue_plus = b - (r+g)/2.0
+				# 	# # write transitions
+				# 	# white_tr = '_'.join(map(str, white[pixel_index][:4].astype(int)))
+				# 	# black_tr = '_'.join(map(str, black[pixel_index][:4].astype(int)))
+				# 	# row = [black_tr,white_tr]
+				# 	# writer.writerow(row)
+				# # red
+				# else:
+				# 	if ( (r > b) and (r > g ) ):
+				# 		red_plus = r - (b+g)/2.0	
 
 				# # black
 				# if !((r < b_t) or (g < b_t) or (b < b_t)):
@@ -229,36 +249,35 @@ def black_white_next(image_dir, output_dir):
 				# if !((r > w_t) or (g > w_t) or (b > w_t)):
 				# 	white[pixel_index][2] = 1
 				
-				# # blue
-				# if ( ((b - blue_t) > r) and ((b - blue_t) > g )):
-				# 	blue_plus = b - (r+g)/2.0
-				# # red
-				# else:
-				# 	if (((r - red_t) > b) and ((r - red_t) > g )):
-				# 		red_plus = r - (b+g)/2.0	
+				# blue
+				if ( ((b - blue_t) > r) and ((b - blue_t) > g )):
+					blue_plus = b - (r+g)/2.0
+				# red
+				else:
+					if (((r - red_t) > b) and ((r - red_t) > g )):
+						red_plus = r - (b+g)/2.0	
 
-
-				# check time transitions
+				# #check time transitions
 				if (black[pixel_index][0] == 1 and white[pixel_index][1] == 1):
 					bw_sum = bw_sum + 1
 					red_sum[0] = red_sum[0] + red_plus
 					blue_sum[0] = blue_sum[0] + blue_plus
-					# new_image[i,j,0] = r
-					# new_image[i,j,1] = 0
-					# new_image[i,j,2] = b
+					new_image[i,j,0] = r
+					new_image[i,j,1] = 0
+					new_image[i,j,2] = b
 
 				if (white[pixel_index][0] == 1 and black[pixel_index][1] == 1):
 					wb_sum = wb_sum + 1
 					red_sum[1] = red_sum[1] + red_plus
 					blue_sum[1] = blue_sum[1] + blue_plus
-					# new_image[i,j,0] = r
-					# new_image[i,j,1] = 0
-					# new_image[i,j,2] = b
+					new_image[i,j,0] = r
+					new_image[i,j,1] = 0
+					new_image[i,j,2] = b
 
-				# if (black[pixel_index][0] == 1 
-				# 	and white[pixel_index][1] == 1
-				# 	and black[pixel_index][3] == 1
-				# 	and white[pixel_index][4] == 1
+				# if (white[pixel_index][0] == 1 
+				# 	and black[pixel_index][1] == 1
+				# 	and black[pixel_index][2] == 1
+				# 	#and white[pixel_index][4] == 1
 				# 	):
 				# 	bw_sum = bw_sum + 1
 				# 	red_sum[0] = red_sum[0] + red_plus
@@ -267,9 +286,22 @@ def black_white_next(image_dir, output_dir):
 				# 	new_image[i,j,1] = 0
 				# 	new_image[i,j,2] = b
 				# if (white[pixel_index][0] == 1 
+				# 	and white[pixel_index][1] == 1
+				# 	and white[pixel_index][2] == 1
+				# 	and white[pixel_index][3] == 0
+				# 	and black[pixel_index][0] == 0
+				# 	and black[pixel_index][1] == 0
+				# 	and black[pixel_index][2] == 0
+				# 	and black[pixel_index][3] == 0
+				# 	):
+				# if (white[pixel_index][0] == 0 
+				# 	and white[pixel_index][1] == 0
+				# 	and white[pixel_index][2] == 0
+				# 	and white[pixel_index][3] == 0
+				# 	and black[pixel_index][0] == 1
 				# 	and black[pixel_index][1] == 1
-				# 	and white[pixel_index][3] == 1
-				# 	and black[pixel_index][4] == 1
+				# 	and black[pixel_index][2] == 1
+				# 	and black[pixel_index][3] == 0
 				# 	):
 				# 	wb_sum = wb_sum + 1
 				# 	red_sum[1] = red_sum[1] + red_plus
@@ -280,10 +312,10 @@ def black_white_next(image_dir, output_dir):
 
 				pixel_index = pixel_index + 1
 
-			# image_array = Image.fromarray(new_image.astype('uint8'), 'RGB')
-			# name = output_dir + image_file
-			# image_array.save(name)
-			# print("saved image ", name)
+			image_array = Image.fromarray(new_image.astype('uint8'), 'RGB')
+			name = output_dir + "/bike_15m/" + image_file
+			image_array.save(name)
+			print("saved image ", name)
 
 
 			# (black->white , white->black)
