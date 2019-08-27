a905270a850cb9a9902a971bcefab36f6707e74b
Lana <lana.sinapayen@gmail.com>
2019-08-27 14:57:41 +0900

    add start argument

--- verify_predicted_colors.py
+++ verify_predicted_colors.py
@@ -51,16 +51,23 @@ def color_diff(image_path):
 
 	return(new_image)
 
+# colors = predicted colors, col_image = actual colors
 def match(colors, col_image):
 	new_image = np.zeros(colors.shape)
 
 	for i in xrange(0,colors.shape[0]):
 		for j in xrange(0,colors.shape[1]):
-			# print(col_image[i, j])
-			# print(colors[i, j])
-			strong_color = strong_index(col_image[i, j])
-			if strong_color != -1 and colors[i,j,strong_color] != 0:
-				new_image[i,j,strong_color] = col_image[i,j,strong_color]
+
+			# compare if strong colors are the same
+			# strong_color = strong_index(col_image[i, j])
+			# if strong_color != -1 and colors[i,j,strong_color] != 0:
+				# new_image[i,j,strong_color] = col_image[i,j,strong_color]
+
+			# check if predicted intensity is correct
+			for c in xrange(0,3):
+				diff = (colors[i,j,c]-col_image[i,j,c])
+				if ( abs(diff) < 50):
+					new_image[i,j,c] = col_image[i,j,c]
 
 	return(new_image)
 
@@ -169,7 +176,7 @@ def process_increases(image_dir, col_image_dir, gs_image_dir, image_count, outpu
 
 # col_image_dir = directory with original color images used as input
 # gs_image_dir = directory with original greyscale images used as input
-def process_images(image_dir, col_image_dir, gs_image_dir, image_count, output_dir):
+def process_images(image_dir, col_image_dir, gs_image_dir, image_count, output_dir, start):
 	# loop on images
 	image_list = sorted(os.listdir(image_dir))
 	gs_image_list = sorted(os.listdir(gs_image_dir))
@@ -181,16 +188,15 @@ def process_images(image_dir, col_image_dir, gs_image_dir, image_count, output_d
 	diff_dir = output_dir + "/col_diff/"
 	if not os.path.exists(diff_dir):
 		os.makedirs(diff_dir)
-	match_dir = output_dir + "/col_match_increase/"
+	match_dir = output_dir + "/col_match/"
 	if not os.path.exists(match_dir):
 		os.makedirs(match_dir)
-	overlay_dir = output_dir + "/col_match_overlay/"
+	overlay_dir = output_dir + "/col_overlay/"
 	if not os.path.exists(overlay_dir):
 		os.makedirs(overlay_dir)
 
 	im_index = 0
-	col_images = []
-	for image_file in image_list[:image_count]:
+	for image_file in image_list[start:start+image_count]:
 		image_path = os.path.join(image_dir, image_file)
 		print("read ", image_path)
 
@@ -202,46 +208,28 @@ def process_images(image_dir, col_image_dir, gs_image_dir, image_count, output_d
 		image_array.save(name)
 		print("saved image ", name)
 
-		# # check if colors match with original color input
-		# col_image_path = os.path.join(col_image_dir, col_image_list[im_index])
-		# # print("col_image_path ", col_image_path)
-		# col_image = np.array(Image.open(col_image_path).convert('RGB'))
-		# matching_colors = match(colors, col_image) 
-		# # save it
-		# image_array = Image.fromarray(matching_colors.astype('uint8'), 'RGB')
-		# name = match_dir + image_file
-		# image_array.save(name)
-		# print("saved image ", name)
-
-
-		col_image_path = os.path.join(col_image_dir, col_image_list[im_index])
+		# check if colors match with original color input
+		col_image_path = os.path.join(col_image_dir, col_image_list[start+im_index])
+		# print("col_image_path ", col_image_path)
 		col_image = np.array(Image.open(col_image_path).convert('RGB'))
-		if(im_index>0):
-			# check if colors match with color increase in original input
-			col_images[1] = col_image
-			matching_colors = match_increase(colors, col_images) 
-			# save it
-			image_array = Image.fromarray(matching_colors.astype('uint8'), 'RGB')
-			name = match_dir + image_file
-			image_array.save(name)
-			print("saved image ", name)
+		matching_colors = match(colors, col_image) 
+		# save it
+		image_array = Image.fromarray(matching_colors.astype('uint8'), 'RGB')
+		name = match_dir + image_file
+		image_array.save(name)
+		print("saved image ", name)
 
-			# paste color on black and white input
-			gs_image_path = os.path.join(gs_image_dir, gs_image_list[im_index])
-			gs_image = np.array(Image.open(gs_image_path).convert('RGB'))
-			overlay_image = overlay(matching_colors, gs_image) 
-			# save it
-			image_array = Image.fromarray(overlay_image.astype('uint8'), 'RGB')
-			name = overlay_dir + image_file
-			image_array.save(name)
-			print("saved image ", name)
-		else:
-			# preallocate size
-			col_images.append(col_image)
-			col_images.append(col_image)
+		# paste color on black and white input
+		gs_image_path = os.path.join(gs_image_dir, gs_image_list[start+im_index])
+		gs_image = np.array(Image.open(gs_image_path).convert('RGB'))
+		overlay_image = overlay(matching_colors, gs_image) 
+		# save it
+		image_array = Image.fromarray(overlay_image.astype('uint8'), 'RGB')
+		name = overlay_dir + image_file
+		image_array.save(name)
+		print("saved image ", name)
 
 		im_index = im_index + 1
-		col_images[0] = col_image
 
 
 parser = argparse.ArgumentParser(description='image_analysis')
@@ -251,10 +239,12 @@ parser.add_argument('gs_image_dir', action='store', nargs='?', help='Path to ori
 parser.add_argument('output_dir', action='store', nargs='?', help='output directory')
 parser.add_argument('n_images', action='store', nargs='?', default=-1,
                     type=int, help='optional: total number of images to use.')
+parser.add_argument('--start', '-', default=0, type=int, help='where in the image list to start the analysis')
+
 args = parser.parse_args()
 
 if not os.path.exists(args.output_dir):
     os.makedirs(args.output_dir)
 
-#process_images(args.image_dir, args.col_image_dir, args.gs_image_dir, args.n_images, args.output_dir)
-process_increases(args.image_dir, args.col_image_dir, args.gs_image_dir, args.n_images, args.output_dir)
+process_images(args.image_dir, args.col_image_dir, args.gs_image_dir, args.n_images, args.output_dir, args.start)
+#process_increases(args.image_dir, args.col_image_dir, args.gs_image_dir, args.n_images, args.output_dir)
