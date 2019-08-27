a2805e9a63d0e798a9e8735289e20532adec1bf6
Lana <lana.sinapayen@gmail.com>
2019-08-27 15:30:13 +0900

    add padding option to batch rename

--- batch_rename.py
+++ batch_rename.py
@@ -2,13 +2,14 @@ import argparse
 import os
 
 parser = argparse.ArgumentParser(description='image_analysis')
-parser.add_argument('image_dir', action='store', nargs='?', help='Path to prednet output images')
+parser.add_argument('image_dir', help='Path to prednet output images')
+parser.add_argument('--padding', "--p",  default=4, type=int, help='Number of padding 0s')
 args = parser.parse_args()
 
 path = args.image_dir
 for filename in os.listdir(path):
     num = filename.split('_')[1].split('y')[0]
     print(num)
-    num = num.zfill(4)
+    num = num.zfill(args.padding)
     new_filename = num + ".png"
     os.rename(os.path.join(path, filename), os.path.join(path, new_filename))
