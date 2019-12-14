357d1d4290f37f7526d7e9e09a79d572755c5a42
Lana <lana.sinapayen@gmail.com>
2019-12-14 22:36:33 +0900

    record previous pixel value

--- compare_scripts/colored_pixels.py
+++ compare_scripts/colored_pixels.py
@@ -6,7 +6,7 @@ from PIL import Image
 
 # save the position and values of >0 pixels
 def record(input_path, output_dir, limit):
-    csv_file = output_dir + "/pixels.csv"
+    write_file = output_dir + "/pixels.csv"
     print("Writing in", csv_file)
     w = 160
     h = 120
@@ -17,11 +17,11 @@ def record(input_path, output_dir, limit):
     else:
         n = limit
 
-
-    with open(csv_file, mode='w') as csv_file:
+    with open(write_file, mode='w') as csv_file:
         fieldnames = ["image","x", "y", "r", "g", "b"]
         writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(fieldnames)
+
         for i in range(0,n):
             input_image_path = input_path + "/" + input_list[i]
             input_image = np.array(Image.open(input_image_path).convert('RGB'))
@@ -32,6 +32,33 @@ def record(input_path, output_dir, limit):
                         row = [str(i),str(x),str(y)]
                         row.extend(input_image[y,x])
                         writer.writerow(row)
+
+# record the previous value of the pixels
+def record_previous(input_path, output_dir):
+    input_list = sorted(os.listdir(input_path))
+
+    read_file = output_dir + "/pixels.csv"
+    write_file = output_dir + "/previous_pixels.csv"
+    with open(read_file, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        next(reader, None) # skip header
+        with open(write_file, mode='w') as csv_file:
+            fieldnames = ["image","x", "y", "r", "g", "b"]
+            writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow(fieldnames)
+            for row in reader:
+                i = int(row[0])
+                if(i==0):
+                    continue
+
+                # get pixel from coordinates
+                input_image_path = input_path + "/" + input_list[i-1]
+                input_image = np.array(Image.open(input_image_path).convert('RGB'))
+                pixel = input_image[int(row[2]), int(row[1])]
+                # write in file
+                row2 = row[0:2]
+                row.extend(pixel)
+                writer.writerow(row)
 
 
 parser = argparse.ArgumentParser(description='image_copy')
@@ -44,4 +71,6 @@ output_dir = args.output_dir
 if not os.path.exists(output_dir):
   os.makedirs(output_dir)
 
-record(args.input, output_dir, args.limit)
+#record(args.input, output_dir, args.limit)
+record_previous(args.input, output_dir)
+
