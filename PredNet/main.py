a4200856081366870cdf224cd2eb8b229a9f442c
Lana <lana.sinapayen@gmail.com>
2019-12-25 15:21:43 +0900

    implementing optical flow tests

--- PredNet/main.py
+++ PredNet/main.py
@@ -14,6 +14,204 @@ import chainer.computational_graph as c
 from tb_chainer import SummaryWriter, NodeName, utils
 import net
 
+def call_prednet(args):
+    if (not args.images) and (not args.sequences):
+        print('Please specify images or sequences')
+        exit()
+
+    if (not args.test):
+        images_path = os.path.join(args.images, "train_list" + ".txt")
+    else:
+        images_path = os.path.join(args.images, "test_list" + ".txt")
+
+    args.size = args.size.split(',')
+    for i in range(len(args.size)):
+        args.size[i] = int(args.size[i])
+    args.channels = args.channels.split(',')
+    for i in range(len(args.channels)):
+        args.channels[i] = int(args.channels[i])
+    args.offset = args.offset.split(',')
+    for i in range(len(args.offset)):
+        args.offset[i] = int(args.offset[i])
+
+    if args.gpu >= 0:
+        cuda.check_cuda_available()
+    xp = cuda.cupy if args.gpu >= 0 else np
+
+    #Create Model
+    prednet = net.PredNet(args.size[0], args.size[1], args.channels)
+    model = L.Classifier(prednet, lossfun=mean_squared_error)
+    model.compute_accuracy = False
+    optimizer = optimizers.Adam()
+    optimizer.setup(model)
+
+    if args.gpu >= 0:
+        cuda.get_device(args.gpu).use()
+        model.to_gpu()
+        print('Running on a GPU')
+    else:
+        print('Running on a CPU')
+
+    # Init/Resume
+    if args.initmodel:
+        print('Load model from', args.initmodel)
+        serializers.load_npz(args.initmodel, model)
+    if args.resume:
+        print('Load optimizer state from', args.resume)
+        serializers.load_npz(args.resume, optimizer)
+
+    if not os.path.exists('models'):
+        os.makedirs('models')
+    if not os.path.exists('images'):
+        os.makedirs('images')
+    if not os.path.exists('result'):
+        os.makedirs('result')
+
+    def load_list(path, root):
+        tuples = []
+        for line in open(path):
+            pair = line.strip().split()
+            tuples.append(os.path.join(root, pair[0]))
+        return tuples
+
+    def read_image(path):
+        full_path = os.path.join(args.images, path)
+        image = np.asarray(Image.open(full_path)).transpose(2, 0, 1)
+        # // is int division
+        top = args.offset[1] + (image.shape[1]  - args.size[1]) // 2
+        left = args.offset[0] + (image.shape[2]  - args.size[0]) // 2
+        bottom = args.size[1] + top
+        right = args.size[0] + left
+        image = image[:, top:bottom, left:right].astype(np.float32)
+        image /= 255
+        return image
+
+    def write_image(image, path):
+        image *= 255
+        image = image.transpose(1, 2, 0)
+        image = image.astype(np.uint8)
+        result = Image.fromarray(image)
+        result.save(path)
+
+    if args.images:
+        sequencelist = [images_path]
+    else:
+        sequencelist = load_list(args.sequences, args.root)
+
+    writer = SummaryWriter('runs/test')#+datetime.now().strftime('%B%d  %H:%M:%S'))
+
+    def save_model(count):
+        print('save the model')
+        serializers.save_npz('models/' + str(count) + '.model', model)
+        print('save the optimizer')
+        serializers.save_npz('models/' + str(count) + '.state', optimizer)
+        for name, param in model.predictor.namedparams():
+            writer.add_histogram(name, chainer.cuda.to_cpu(param.data), count)
+        writer.add_scalar('loss', float(model.loss.data), count)
+
+    if args.test == True:
+        for seq in range(len(sequencelist)):
+            imagelist = load_list(sequencelist[seq], args.root)
+            prednet.reset_state()
+            loss = 0
+            batchSize = 1
+            x_batch = np.ndarray((batchSize, args.channels[0], args.size[1], args.size[0]), dtype=np.float32)
+            y_batch = np.ndarray((batchSize, args.channels[0], args.size[1], args.size[0]), dtype=np.float32)
+            for i in range(0, len(imagelist)):
+                print("seq ", seq," frame ", imagelist[i])
+                x_batch[0] = read_image(imagelist[i])
+                loss += model(chainer.Variable(xp.asarray(x_batch)),
+                              chainer.Variable(xp.asarray(y_batch)))
+                loss.unchain_backward()
+                loss = 0
+                if args.gpu >= 0:model.to_cpu()
+                #write_image(x_batch[0].copy(), 'result/test_' + str(i) + 'x.png')
+                num = str(i).zfill(10)
+                new_filename = 'result/' + num + '.png'
+                write_image(model.y.data[0].copy(), new_filename)
+                if args.gpu >= 0: model.to_gpu()
+
+                # if i == 0 or (args.input_len > 0 and i % args.input_len != 0):
+                #     continue
+                if i == 0  or (i%args.ext_t>0) or (args.ext==0):
+                    continue
+
+                if args.gpu >= 0: model.to_cpu()
+                x_batch[0] = model.y.data[0].copy()
+                if args.gpu >= 0: model.to_gpu()
+                print(args.ext)
+                for j in range(0,args.ext):
+                    print('extended frameNo:' + str(j + 1))
+                    loss += model(chainer.Variable(xp.asarray(x_batch)),
+                                  chainer.Variable(xp.asarray(y_batch)))
+                    if j == args.ext - 1:
+                        g = c.build_computational_graph([model.y])
+                        node_name = NodeName(g.nodes)
+                        for n in g.nodes:
+                            if isinstance(n, chainer.variable.VariableNode) and \
+                              not isinstance(n._variable(), chainer.Parameter) and n.data is not None:
+                                img = utils.make_grid(np.expand_dims(chainer.cuda.to_cpu(n.data[-1, ...]), 1))
+                                writer.add_image(node_name.name(n), img, i)
+                    loss.unchain_backward()
+                    loss = 0
+                    if args.gpu >= 0:model.to_cpu()
+                    write_image(model.y.data[0].copy(), 'result/test_' + str(i) + 'y_' + str(j + 1) + '.png')
+                    x_batch[0] = model.y.data[0].copy()
+                    if args.gpu >= 0:model.to_gpu()
+                prednet.reset_state()
+    else:
+        logf = open('log.txt', 'w')
+        count = 0
+        seq = 0
+        while count < args.period:
+            imagelist = load_list(sequencelist[seq], args.root)
+            prednet.reset_state()
+            loss = 0
+
+            batchSize = 1
+            x_batch = np.ndarray((batchSize, args.channels[0], args.size[1], args.size[0]), dtype=np.float32)
+            y_batch = np.ndarray((batchSize, args.channels[0], args.size[1], args.size[0]), dtype=np.float32)
+            if len(imagelist) == 0:
+                print("Not found images.")
+                break
+            x_batch[0] = read_image(imagelist[0])
+            for i in range(1, len(imagelist)):
+                y_batch[0] = read_image(imagelist[i]);
+                loss += model(chainer.Variable(xp.asarray(x_batch)),
+                              chainer.Variable(xp.asarray(y_batch)))
+
+                if (i + 1) % args.bprop == 0:
+                    print("count ", count," frameNo ", i)
+                    model.zerograds()
+                    loss.backward()
+                    loss.unchain_backward()
+                    loss = 0
+                    optimizer.update()
+                    if args.gpu >= 0:model.to_cpu()
+                    # write_image(x_batch[0].copy(), 'images/' + str(count) + '_' + str(seq) + '_' + str(i) + 'x.png')
+                    # write_image(model.y.data[0].copy(), 'images/' + str(count) + '_' + str(seq) + '_' + str(i) + 'y.png')
+                    # write_image(y_batch[0].copy(), 'images/' + str(count) + '_' + str(seq) + '_' + str(i) + 'z.png')
+                    if args.gpu >= 0:model.to_gpu()
+                    print('loss:' + str(float(model.loss.data)))
+                    logf.write(str(i) + ', ' + str(float(model.loss.data)) + '\n')
+
+                if (count%args.save) == 0:
+                    save_model(count)
+                x_batch[0] = y_batch[0]
+                count += 1
+                if (count>=args.period):
+                    save_model(count)
+                    break
+
+            seq = (seq + 1)%len(sequencelist)
+
+    # For logging graph structure
+    model(chainer.Variable(xp.asarray(x_batch)),
+          chainer.Variable(xp.asarray(y_batch)))
+    writer.add_graph(model.y)
+    writer.close()
+
+
 parser = argparse.ArgumentParser(
 description='PredNet')
 parser.add_argument('--images', '-i', default='', help='Path to image list file')
@@ -48,199 +246,4 @@ parser.add_argument('--test', dest='test', action='store_true')
 parser.set_defaults(test=False)
 args = parser.parse_args()
 
-
-if (not args.images) and (not args.sequences):
-    print('Please specify images or sequences')
-    exit()
-
-if (not args.test):
-    images_path = os.path.join(args.images, "train_list" + ".txt")
-else:
-    images_path = os.path.join(args.images, "test_list" + ".txt")
-
-args.size = args.size.split(',')
-for i in range(len(args.size)):
-    args.size[i] = int(args.size[i])
-args.channels = args.channels.split(',')
-for i in range(len(args.channels)):
-    args.channels[i] = int(args.channels[i])
-args.offset = args.offset.split(',')
-for i in range(len(args.offset)):
-    args.offset[i] = int(args.offset[i])
-
-if args.gpu >= 0:
-    cuda.check_cuda_available()
-xp = cuda.cupy if args.gpu >= 0 else np
-
-#Create Model
-prednet = net.PredNet(args.size[0], args.size[1], args.channels)
-model = L.Classifier(prednet, lossfun=mean_squared_error)
-model.compute_accuracy = False
-optimizer = optimizers.Adam()
-optimizer.setup(model)
-
-if args.gpu >= 0:
-    cuda.get_device(args.gpu).use()
-    model.to_gpu()
-    print('Running on a GPU')
-else:
-    print('Running on a CPU')
-
-# Init/Resume
-if args.initmodel:
-    print('Load model from', args.initmodel)
-    serializers.load_npz(args.initmodel, model)
-if args.resume:
-    print('Load optimizer state from', args.resume)
-    serializers.load_npz(args.resume, optimizer)
-
-if not os.path.exists('models'):
-    os.makedirs('models')
-if not os.path.exists('images'):
-    os.makedirs('images')
-if not os.path.exists('result'):
-    os.makedirs('result')
-
-def load_list(path, root):
-    tuples = []
-    for line in open(path):
-        pair = line.strip().split()
-        tuples.append(os.path.join(root, pair[0]))
-    return tuples
-
-def read_image(path):
-    full_path = os.path.join(args.images, path)
-    image = np.asarray(Image.open(full_path)).transpose(2, 0, 1)
-    # // is int division
-    top = args.offset[1] + (image.shape[1]  - args.size[1]) // 2
-    left = args.offset[0] + (image.shape[2]  - args.size[0]) // 2
-    bottom = args.size[1] + top
-    right = args.size[0] + left
-    image = image[:, top:bottom, left:right].astype(np.float32)
-    image /= 255
-    return image
-
-def write_image(image, path):
-    image *= 255
-    image = image.transpose(1, 2, 0)
-    image = image.astype(np.uint8)
-    result = Image.fromarray(image)
-    result.save(path)
-
-if args.images:
-    sequencelist = [images_path]
-else:
-    sequencelist = load_list(args.sequences, args.root)
-
-writer = SummaryWriter('runs/test')#+datetime.now().strftime('%B%d  %H:%M:%S'))
-
-def save_model(count):
-    print('save the model')
-    serializers.save_npz('models/' + str(count) + '.model', model)
-    print('save the optimizer')
-    serializers.save_npz('models/' + str(count) + '.state', optimizer)
-    for name, param in model.predictor.namedparams():
-        writer.add_histogram(name, chainer.cuda.to_cpu(param.data), count)
-    writer.add_scalar('loss', float(model.loss.data), count)
-
-if args.test == True:
-    for seq in range(len(sequencelist)):
-        imagelist = load_list(sequencelist[seq], args.root)
-        prednet.reset_state()
-        loss = 0
-        batchSize = 1
-        x_batch = np.ndarray((batchSize, args.channels[0], args.size[1], args.size[0]), dtype=np.float32)
-        y_batch = np.ndarray((batchSize, args.channels[0], args.size[1], args.size[0]), dtype=np.float32)
-        for i in range(0, len(imagelist)):
-            print("seq ", seq," frame ", imagelist[i])
-            x_batch[0] = read_image(imagelist[i])
-            loss += model(chainer.Variable(xp.asarray(x_batch)),
-                          chainer.Variable(xp.asarray(y_batch)))
-            loss.unchain_backward()
-            loss = 0
-            if args.gpu >= 0:model.to_cpu()
-            #write_image(x_batch[0].copy(), 'result/test_' + str(i) + 'x.png')
-            num = str(i).zfill(10)
-            new_filename = 'result/' + num + '.png'
-            write_image(model.y.data[0].copy(), new_filename)
-            if args.gpu >= 0: model.to_gpu()
-
-            # if i == 0 or (args.input_len > 0 and i % args.input_len != 0):
-            #     continue
-            if i == 0  or (i%args.ext_t>0) or (args.ext==0):
-                continue
-
-            if args.gpu >= 0: model.to_cpu()
-            x_batch[0] = model.y.data[0].copy()
-            if args.gpu >= 0: model.to_gpu()
-            print(args.ext)
-            for j in range(0,args.ext):
-                print('extended frameNo:' + str(j + 1))
-                loss += model(chainer.Variable(xp.asarray(x_batch)),
-                              chainer.Variable(xp.asarray(y_batch)))
-                if j == args.ext - 1:
-                    g = c.build_computational_graph([model.y])
-                    node_name = NodeName(g.nodes)
-                    for n in g.nodes:
-                        if isinstance(n, chainer.variable.VariableNode) and \
-                          not isinstance(n._variable(), chainer.Parameter) and n.data is not None:
-                            img = utils.make_grid(np.expand_dims(chainer.cuda.to_cpu(n.data[-1, ...]), 1))
-                            writer.add_image(node_name.name(n), img, i)
-                loss.unchain_backward()
-                loss = 0
-                if args.gpu >= 0:model.to_cpu()
-                write_image(model.y.data[0].copy(), 'result/test_' + str(i) + 'y_' + str(j + 1) + '.png')
-                x_batch[0] = model.y.data[0].copy()
-                if args.gpu >= 0:model.to_gpu()
-            prednet.reset_state()
-else:
-    logf = open('log.txt', 'w')
-    count = 0
-    seq = 0
-    while count < args.period:
-        imagelist = load_list(sequencelist[seq], args.root)
-        prednet.reset_state()
-        loss = 0
-
-        batchSize = 1
-        x_batch = np.ndarray((batchSize, args.channels[0], args.size[1], args.size[0]), dtype=np.float32)
-        y_batch = np.ndarray((batchSize, args.channels[0], args.size[1], args.size[0]), dtype=np.float32)
-        if len(imagelist) == 0:
-            print("Not found images.")
-            break
-        x_batch[0] = read_image(imagelist[0])
-        for i in range(1, len(imagelist)):
-            y_batch[0] = read_image(imagelist[i]);
-            loss += model(chainer.Variable(xp.asarray(x_batch)),
-                          chainer.Variable(xp.asarray(y_batch)))
-
-            if (i + 1) % args.bprop == 0:
-                print("count ", count," frameNo ", i)
-                model.zerograds()
-                loss.backward()
-                loss.unchain_backward()
-                loss = 0
-                optimizer.update()
-                if args.gpu >= 0:model.to_cpu()
-                # write_image(x_batch[0].copy(), 'images/' + str(count) + '_' + str(seq) + '_' + str(i) + 'x.png')
-                # write_image(model.y.data[0].copy(), 'images/' + str(count) + '_' + str(seq) + '_' + str(i) + 'y.png')
-                # write_image(y_batch[0].copy(), 'images/' + str(count) + '_' + str(seq) + '_' + str(i) + 'z.png')
-                if args.gpu >= 0:model.to_gpu()
-                print('loss:' + str(float(model.loss.data)))
-                logf.write(str(i) + ', ' + str(float(model.loss.data)) + '\n')
-
-            if (count%args.save) == 0:
-                save_model(count)
-            x_batch[0] = y_batch[0]
-            count += 1
-            if (count>=args.period):
-                save_model(count)
-                break
-
-        seq = (seq + 1)%len(sequencelist)
-
-# For logging graph structure
-model(chainer.Variable(xp.asarray(x_batch)),
-      chainer.Variable(xp.asarray(y_batch)))
-writer.add_graph(model.y)
-writer.close()
+call_prednet(args)
---
+++ optical_flow_tests.py
@@ -0,0 +1,45 @@
+import sys
+
+#sys.path.insert(1, '/PredNet')
+#import main as prednet_main
+from PredNet.main import call_prednet
+from generate_imagelist import generate as generate_imagelist
+
+
+
+# process images as static images
+def predict_static(input_path, model_name, limit):
+	# how many times to repeat each image
+	repeat = 10
+
+	# create image list
+	# 'chainer_prednet/generate_imagelist.py' 'imported/' '1' -tr 10
+	imagelist_args = {"data_dir": input_path, "n_images": limit, "r": repeat}
+	generate_imagelist(imagelist_args)
+
+
+	# %run 'chainer_prednet/PredNet/main.py' --images 'imported' --initmodel 'fpsi_500000_20v.model' --input_len 10 --test 
+	l = limit*10
+	prednet_args = {"images":input_path, "initmodel":model_name, "input_len":l, "test":0}
+	call_prednet(prednet_args)
+	
+
+# calculate optical flow compared to input
+
+
+# do the mirror test
+
+
+parser = argparse.ArgumentParser(description='optical flow tests')
+parser.add_argument('--input', '-i', default='', help='Path to input directory')
+parser.add_argument('--model', '-m', default='', help='.model file')
+parser.add_argument('--output_dir', '-o', default='', help='path of output diectory')
+parser.add_argument('--limit', '-l', type=int, default=0, help='max number of images')
+
+
+args = parser.parse_args()
+output_dir = args.output_dir 
+if not os.path.exists(output_dir):
+  os.makedirs(output_dir)
+
+predict_static(args.input, args.model, args.limit)
