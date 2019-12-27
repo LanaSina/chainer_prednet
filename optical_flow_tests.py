0fba12d086212e3935fc8ba21d515cea7b7ce457
Lana <lana.sinapayen@gmail.com>
2019-12-27 16:56:38 +0900

    add repeat option

--- optical_flow_tests.py
+++ optical_flow_tests.py
@@ -80,4 +80,4 @@ output_dir = args.output_dir
 if not os.path.exists(output_dir):
   os.makedirs(output_dir)
 
-predict_static(args.input,output_dir, args.model, args.limit)
+predict_static(args.input,output_dir, args.model, args.limit, args.repeat)
